6252bd0dae696f8f59997a1ac4a955a4d48d6692
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2017-03-21 23:58:45 -0500

    Verde: Refactor

--- Numeros.py
+++ Numeros.py
@@ -5,7 +5,7 @@ _author_ = 'ji.bernal27 oa.fajardo'
 class Numeros:
     def mmne(self, cadena):
         if cadena == "":
-            return [0, 0, 0, 0]
+            return [0]*4
         else:
             numeroosPar = [float(i) for i in cadena.split(",")]
             return [len(numeroosPar), min(numeroosPar), max(numeroosPar), sum(numeroosPar) / float(len(numeroosPar))]

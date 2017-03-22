0b8e3c5c21a6ab329348d05ca7f00b7ffeb905ab
Oscar Andres Fajardo <oscar8500@gmail.com>
2017-03-21 22:51:41 -0500

    Un elemento Maximo Rojo

--- Numeros.py
+++ Numeros.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-_author_='ji.bernal27 oa.fajardo'
+_author_ = 'ji.bernal27 oa.fajardo'
+
 
 class Numeros:
-    def mmne(self,cadena):
-        if cadena=="":
+    def mmne(self, cadena):
+        if cadena == "":
             return [0, 0, 0, 0]
         else:
-            return [1,float(cadena)]
+            return [1, float(cadena)]
--- NumerosTEST.py
+++ NumerosTEST.py
@@ -2,13 +2,18 @@
 from unittest import TestCase
 
 from  Numeros import Numeros
+
+
 class NumerosTEST(TestCase):
     def test_NumeroElementos(self):
         self.assertEqual(Numeros().mmne("")[0], 0, "Número de elementos")
+
     def test_Minimo(self):
         self.assertEqual(Numeros().mmne("")[1], 0, "Mínimo")
+
     def test_Maximo(self):
         self.assertEqual(Numeros().mmne("")[2], 0, "Máximo")
+
     def test_Promedio(self):
         self.assertEqual(Numeros().mmne("")[3], 0, "Promedio")
 
@@ -16,4 +21,7 @@ class NumerosTEST(TestCase):
         self.assertEqual(Numeros().mmne("1")[0], 1, "Número de elementos")
 
     def test_Minimo1(self):
-        self.assertEqual(Numeros().mmne("1")[1], 1, "Número de elementos")
+        self.assertEqual(Numeros().mmne("1")[1], 1, "Mínimo")
+
+    def test_Maximo1(self):
+        self.assertEqual(Numeros().mmne("1")[2], 1, "Máximo")

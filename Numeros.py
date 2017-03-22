efe0cbb79c8886f1fd25593a3d0fdf456b543ca4
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2017-03-21 23:42:56 -0500

    Verde: Mínmo  cadena con string de varios número números

--- Numeros.py
+++ Numeros.py
@@ -9,5 +9,5 @@ class Numeros:
         elif not "," in cadena:
             return [float(cadena), float(cadena), float(cadena), float(cadena)]
         else:
-            numeros = cadena.split(",")
-            return [len(numeros), 1, 2, 1.5]
+            numeroosPar=[float(i) for i in cadena.split(",")]
+            return [len(numeroosPar), min(numeroosPar), 2, 1.5]

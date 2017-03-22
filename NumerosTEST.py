d1d6eabb0ad9c841ef2c948d25be2e38971f5891
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2017-03-21 22:03:35 -0500

    Rojo: Mínimo cadena vacia

--- NumerosTEST.py
+++ NumerosTEST.py
@@ -5,3 +5,5 @@ from  Numeros import Numeros
 class NumerosTEST(TestCase):
     def test_NumeroElementos(self):
         self.assertEqual(Numeros().mmne(""), 0, "Número de elementos")
+    def test_Minimo(self):
+        self.assertEqual(Numeros().mmne("")[1], 0, "mínimo")
